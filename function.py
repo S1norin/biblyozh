5bdf57eff4c0d796e4973e0b6d80603891d79869
liona <lioonalexandrov@gmail.com>
2024-03-29 00:26:14 +0700

    в функцию для чтения файлов txt и pdf. добавил чтение файлов. теперь возвращает кортеж из списка страниц и из списка картинок на первой странице(работоспособность примерная)

--- function.py
+++ function.py
@@ -1,12 +1,24 @@
 import PyPDF2
 
 def file_handler(name, n):
+    images = []
     if name.split(".")[-1] == "txt":
         x = open(name, mode="r")
         string = x.read()
     elif name.split(".")[-1] == "pdf":
         x = open(name, 'rb')
         reader = PyPDF2.PdfReader(x)
+        k = reader.getPage(0)['/Resources']['/XObject'].getObject()
+        for obg in k:
+            if obg[obg]['/Subtipe'] == '/Image':
+                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                data = x0bject[obj].getData()
+                mode = "RGB"
+                if obg[obj]['/Filter'] == '/FlateDecode':
+                    img = Image.frombytes(mode, size, data)
+                else:
+                    print("Error")
+                images.append(img)
         string = ""
         for i in reader.pages:
             string = string + i.extract_text()
@@ -16,4 +28,6 @@ def file_handler(name, n):
         result.append(string[i - n:i])
         if len(string) - n < i:
             result.append(string[i:])
+    if len(images) > 0:
+        result = (result, images)
     return result
