aac5b2047879970811060040afb9925660d1c4fa
LAleksandrov-2022 <lionalexandrov@gmail.com>
2024-04-11 13:48:04 +0300

    updait file_handler function

--- data/data_handlers.py
+++ data/data_handlers.py
@@ -1,35 +1,25 @@
-import PyPDF2
-from PIL import Image
+from bs4 import BeautifulSoup as Soup, Tag
+import lxml
 
+def compile_chapter(section: Tag):
+    title = title_tag.get_text(strip=True) if (
+        title_tag := section.find('title')
+    ) else ''
+    rows = '\n'.join(
+        [p.get_text(strip=True) for p in title_tag.find_next_siblings('p')]
+    )
+    return f'{title}\n{rows}'
 
 def file_handler(name, n):
     images = []
     if name.split(".")[-1] == "txt":
         x = open(name, mode="r")
         string = x.read()
-    elif name.split(".")[-1] == "pdf":
-        x = open(name, 'rb')
-        reader = PyPDF2.PdfReader(x)
-        XObject = reader.pages[0]['/Resources']['/XObject'].getObject()
-        for obg in XObject:
-            if XObject[obg]['/Subtipe'] == '/Image':
-                size = (XObject[obg]['/Width'], XObject[obg]['/Height'])
-                data = XObject[obg].getData()
-                mode = "RGB"
-                if XObject[obg]['/Filter'] == '/FlateDecode':
-                    img = Image.frombytes(mode, size, data)
-                else:
-                    print("Error")
-                images.append(img)
-        string = ""
-        for i in reader.pages:
-            string = string + i.extract_text()
+    elif name.split(".")[-1] == "fb2":
+        with open(name, 'r', encoding='utf-8') as x:
+            soup = Soup(x.read(), 'lxml')
+            string = "\n".join([*map(compile_chapter, soup.find_all('section'))])
     x.close()
-    result = []
-    for i in range(n, len(string), n):
-        result.append(string[i - n:i])
-        if len(string) - n < i:
-            result.append(string[i:])
-    if len(images) > 0:
-        result = (result, images)
-    return result
+    return string
+
+print(file_handler("test_files/test_file_3.fb2", 3))
